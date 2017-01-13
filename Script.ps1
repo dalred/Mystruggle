cb462a3d49c9f9d9ab38e70552864ebacfd9b25d
Daniil <dalred@mail.ru>
2017-01-13 23:49:00 +0300

    Update Script.ps1

--- Script.ps1
+++ Script.ps1
@@ -8,9 +8,24 @@ Param(
     [String]$curDir
 )
 
+  <#
+Copyright ©Даниил Кузьмин,версия: 2017 Январь.По всем вопросам обращаться по почте:Dalred@mail.ru
+
+
+Param(
+    [String]$curDir
+)
+
 $myobject=@()
+$myobject2=@()
+$myobject3=@()
 #$curDir=$PSScriptRoot
 $date1=(Get-Date).ToString('dd.MM.yyyy')
+$EventSys=Get-EventLog -LogName System -After (Get-Date).AddMinutes(-20)
+$EventApp=Get-EventLog -LogName Application -After (Get-Date).AddMinutes(-20)
+$EventDown=Get-EventLog -LogName System  -Source USER32 -After (Get-Date).AddMinutes(-20)| 
+where { @(1076,1074) -contains ($_.InstanceId -bAnd 0xFFFF) }
+
 
 IF ((Test-Path $curDir\log) -ne "True") {
 New-Item  $curDir\log -type directory
@@ -25,12 +40,32 @@ function Service  ($Pr)
     $Pechat=$date+" "+$name+": "+$Pr+$R.DisplayName+" "+"Статус: "+ $R.Status|Out-File -Append "$curDir\log\$date1.txt"
 }
 
+Foreach ($EventDownElement in  $EventDown) 
+{ 
+    If (($EventDownElement.Message -like "*Выключение питания*") -and ($EventDownElement.Message -ne $null))
+        {
+            $properties3 = @{
+            Время=$EventDownElement.TimeWritten;
+            Сообщение="Выключение питания"
+            Источник="Системный журнал:"
+            }
+            $myobject3+=new-object psobject -property $properties3
+        }
+    If (($EventDownElement.Message -like "*Перезапустить*") -and ($EventDownElement.Message -ne $null))      
+    {
+            $properties4 = @{
+            Время=$EventDownElement.TimeWritten;
+            Сообщение="Перезагрузка"
+            Источник="Системный журнал:"
+            }
+            $myobject3+=new-object psobject -property $properties4
+     }
+}
+$myobject3|Sort-Object -Property "Время"|foreach {$_.Время.tostring()+" "+$_.Источник.tostring()+" "+$_.Сообщение.tostring()}|Out-File -Append "$curDir\log\$date1.txt"
+
 Service "Видит: "
 IF ($MyVar -ne "Running")
 {
-    $EventSys=Get-EventLog -LogName System -After (Get-Date).AddMinutes(-20)
-    $EventApp=Get-EventLog -LogName Application -After (Get-Date).AddMinutes(-20)
-
     Foreach ($EventsysElement in $EventSys) 
     { 
         $properties = @{
@@ -51,9 +86,27 @@ IF ($MyVar -ne "Running")
         $myobject+=new-object psobject -property $properties2
 
     }
-    "События за 20 минут до остановки!"|Out-File -Append "$curDir\log\$date1.txt"
-    $myobject|Sort-Object -Property "Время"|foreach {$_.Время.tostring()+" "+$_.Источник.tostring()+" "+$_.Сообщение.tostring()}|Out-File -Append "$curDir\log\$date1.txt"
+    "`t"+"События за 20 минут до остановки!"|Out-File -Append "$curDir\log\$date1.txt"
+    $myobject|Sort-Object -Property "Время" -Unique|foreach {$_.Время.tostring()+" "+$_.Источник.tostring()+" "+$_.Сообщение.tostring()}|Out-File -Append "$curDir\log\$date1.txt"
     Start-Service -name zCSSVC
     Start-Sleep -Milliseconds 1000
 Service "Принудительно запускает(скрипт)- "
+} else {
+$EventSys=Get-EventLog -LogName System -Source "Service Control Manager" -Message "*CryptoServer Service*" -After (Get-Date).AddMinutes(-20)
+    If ($EventSys -ne $null)
+    {
+        Foreach ($EventsysElement in $EventSys) 
+        { 
+            $properties = @{
+            Время=$EventsysElement.TimeWritten;
+            Сообщение=$EventsysElement.Message;
+            Источник="Системный журнал:";
+            }
+        $myobject2+=new-object psobject -property $properties
+        }
+    "`t"+"События которые происходили со службой последние 20 минут в случае ее запуска!"|Out-File -Append "$curDir\log\$date1.txt"
+    $myobject2| Sort-Object -Property "Время" -Unique|where { $_.Сообщение -like "*CryptoServer Service*"}| foreach {"`t"+$_.Время.tostring()+" "+$_.Источник.tostring()+" "+$_.Сообщение.tostring()}|Out-File -Append "$curDir\log\$date1.txt"
+    }
 }
+Exit 
+ 
